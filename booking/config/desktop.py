9953e38cbfbaced30c280cf5797d1bb1305da38d
adit <adit@localhost.localdomain>
2019-04-13 12:43:01 +0700

    Task Print Format, Silakan di cek apakah custom Printnya Kebawa juga ke model Booking nya atau tidak

--- booking/config/desktop.py
+++ booking/config/desktop.py
@@ -6,11 +6,8 @@ def get_data():
 	return [
 		{
 			"module_name": "Booking Room",
-<<<<<<< HEAD
 			"color": "blue",
-=======
 			"color": "red",
->>>>>>> dabde8614dda8e825cd3bc5ed26db928b99f41c5
 			"icon": "octicon octicon-file-directory",
 			"type": "module",
 			"label": _("Booking Room")
